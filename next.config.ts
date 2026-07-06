d89f26f60ef0c4c969aae82ba997cd1648604cd7
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-07-07 00:23:28 +0900

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // GitHub Actions 환경에서 빌드 시 static HTML export를 사용합니다.
+  output: isGithubActions ? "export" : undefined,
+  
+  // GitHub Pages 배포 시 도메인 주소(https://bloodstrawberry.github.io/puzznic)의 서브경로를 지원합니다.
+  basePath: isGithubActions ? "/puzznic" : undefined,
+  
+  // static HTML export 시 내장 이미지 최적화 API를 무시합니다.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
