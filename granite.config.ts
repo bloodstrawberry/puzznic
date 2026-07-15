4af31da8e68af640891b879cd1368ea95e3c4ccc
jandroid.kim <40860674+bloodstrawberry@users.noreply.github.com>
2026-07-16 02:49:56 +0900

    Revert "빌드 설정"
    This reverts commit 45ecb3bba63c949d4e297f4d682bab47e6282fb8.

--- granite.config.ts
+++ granite.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
-  appName: 'puzznic-test',
+  appName: 'blood-puzzle',
   brand: {
     displayName: '블러드 퍼즐', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: '', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
-    host: '192.168.55.120',
+    host: 'localhost',
     port: 5173,
     commands: {
-      dev: 'next dev -p 5173 -H 0.0.0.0',
+      dev: 'next dev',
       build: 'next build',
     },
   },
